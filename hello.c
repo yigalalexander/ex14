47fb9290584fc380b324eb675c96b78fd662b2d8
Yigal Alexander <yigalalexander@Yigals-MacBook-Pro.local>
2013-01-11 13:50:44 +0200

    initial dump of files

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main()
+{
+	printf ("Hello in OSX \n");
+
+}
+
---
+++ main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  hello
+//
+//  Created by Yigal Alexander on 10/8/12.
+//  Copyright (c) 2012 Yigal Alexander. All rights reserved.
+//
+
+#include <stdio.h>
+#include "util.h"
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    int i;
+    for (i=1; i<=250; i++)
+        pnum(i);
+    
+    return 0;
+}
+
