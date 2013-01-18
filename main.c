24d154bac1938d6e402b73248c29c19a9e2264ed
amax1612 <amax1612@hotmail.com>
2013-01-18 12:07:25 -0800

    test

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ int main(int argc, const char * argv[])
     for (i=1; i<=250; i++)
         pnum(i*2);
     printf("Hello, this is another test  to pushing changes in GitHub, yet again");
+    printf("Remote repository test");
     
     return 0;
 }
