dc4270ed126e9f166c1d069118d0a05675273a26
yigalalexander <me@yigalalexander.com>
2013-03-02 00:29:43 +0200

    opcode type definition, first version, using bitfield/ I suggest to use a function to convert it to Int. This will help us avoid doing to much bit shifting

--- src/opcode.c
+++ src/opcode.c
@@ -5,4 +5,19 @@
  *      Author: yigalalexander
  */
 
+typedef struct {
+	unsigned int comb:2;
+	unsigned int dst_reg:3;
+	unsigned int dst_add:2;
+	unsigned int src_reg:3;
+	unsigned int src_add:2;
+	unsigned int command:4;
+	unsigned int type:1;
+	unsigned int reserve:3;
+}opcode;
+
+typedef struct {
+	opcode value;
+	void * next;
+} opcode_node;
 
