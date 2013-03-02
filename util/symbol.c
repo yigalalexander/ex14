cc987780355b8c0567e8f07c2f4065be4d856718
yigalalexander <me@yigalalexander.com>
2013-03-02 16:53:15 +0200

    Flush

--- util/symbol.c
+++ util/symbol.c
@@ -5,4 +5,25 @@
  *      Author: yigalalexander
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int flush_symbols(symbol_node * list)
+{
+	if (list!=NULL)
+	{
+		/* for each node free it, use a temp pointer to the next*/
+		symbol_node * temp;
+
+		while (list->next!=NULL) /*while we have another node*/
+		{
+			temp=list; /*save the pointer for this node
+			list=list->next; /*move one node forward */
+			free(temp); /* free current node */
+		}
+		free(list); /* free the list */
+		return (1); /* finished with success */
+	} else return (0); /* failed */
+}
 
